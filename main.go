087d3d905dc48f6ec63fe76be3f308cc9aa7e1ea
Henry Hogan <40265229+HenryEffiong@users.noreply.github.com>
2023-12-16 19:04:56 +0100

    update

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/fatih/color"
+)
+
+const NumberOfPizzas = 10
+
+var pizzasMade, pizzasFailed, total int
+
+type Producer struct {
+	data chan PizzaOrder
+	quit chan chan error
+}
+
+type PizzaOrder struct {
+	pizzaOrder int
+	message string
+	success bool
+}
+
+func main() {
+	//  seed the random number generator
+	rand.New(rand.NewSource(time.Now().Unix()))
+
+	//  print out a message
+	color.Cyan("The Pizzeria is open for business")
+	color.Cyan("---------------------------------")
+
+	//  create a producer
+
+	//  run the producer in the background
+ 
+	//  create and run consumer
+
+	//  print out the ending message
+}
